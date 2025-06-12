3c8c1b2170405df8a42cf06ecfdbcfbbcbd3251d
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-12 23:22:47 +0530

    Updated Bubblesort java

--- nap/.nap/sort_algos/bubble_sort.java
+++ nap/.nap/sort_algos/bubble_sort.java
@@ -1,6 +1,22 @@
 public class Main{
   public static void sort(int[] nums) {
     boolean swapped;
+    for (int i = 0; i < nums.length - 1; i++) {
+      swapped = false;
 
+      for (int j = 0; j < nums.length - i - 1; j++) {
+        if (nums[j] > nums[j+1]) {
+          int temp = nums[j];
+          nums[j] = nums[j+1];
+          nums[j+1] = temp;
+
+          swapped = true;
+        }
+      }
+
+      if(!swapped){
+        break;
+      }
+    }
   }
 }
