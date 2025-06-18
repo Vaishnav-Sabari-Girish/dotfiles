d5b82638d018d4ad71f1ccbea4d6c0f3bd953fe7
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-18 10:13:30 +0530

    Changed markdown previewer neovim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,10 +1,21 @@
 return {
-  {
+  --[[{
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "echasnovski/mini.nvim", -- Alternative: "echasnovski/mini.icons" or "nvim-tree/nvim-web-devicons"
     },
     opts = {}, -- Configure options if needed
+  },]]
+  --
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
   },
 }
