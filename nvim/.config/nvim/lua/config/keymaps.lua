a79a831e9b08d2f8e1179cdfd4aaf98be531c3f2
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-06-27 23:27:34 +0530

    Added custom keymaps for opening oldfiles and floaterm

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Open recent files with fzf-lua
+vim.keymap.set("n", "<leader>of", require("fzf-lua").oldfiles, { desc = "Open recent files with fzf-lua" })
+
+-- Toggle floating terminal with vim-floaterm
+vim.keymap.set("n", "<leader>ft", ":FloatermToggle<CR>", { desc = "Toggle floating terminal" })
