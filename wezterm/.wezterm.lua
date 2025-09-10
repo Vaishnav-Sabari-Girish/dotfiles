3b7c49db45c7d88ec921949c2ea9f812ff6a241a
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-09-10 21:05:48 +0530

    Updated wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 80
+config.initial_rows = 30
 config.font_size = 18
-config.color_scheme = "nordfox"
+config.color_scheme = "Catppuccin Frappe"
 config.enable_tab_bar = false
 config.font = wezterm.font("JetBrains Mono")
 config.default_cursor_style = "BlinkingBar"
