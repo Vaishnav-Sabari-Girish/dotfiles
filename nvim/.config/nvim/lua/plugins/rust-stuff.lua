47748644a1652b286e75e26d87e758d70c56086b
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-11-27 20:35:17 +0530

    Added rustowl to neovim

--- nvim/.config/nvim/lua/plugins/rust-stuff.lua
+++ nvim/.config/nvim/lua/plugins/rust-stuff.lua
@@ -1,5 +1,7 @@
 return {
-  "mrcjkb/rustaceanvim",
-  version = "^6", -- Recommended
+  "cordx56/rustowl",
+  version = "*", -- Latest stable version
+  build = "cargo binstall rustowl",
   lazy = false, -- This plugin is already lazy
+  opts = {},
 }
