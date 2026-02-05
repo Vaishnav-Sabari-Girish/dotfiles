da61768da06cf1cc6e0ddfe00e8db5fdfc46e7fe
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-02-05 17:42:04 +0530

    feat(rust stuff update): Force stable toolchain
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -3,6 +3,13 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^7", -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          cmd = { "rustup", "run", "stable", "rust-analyzer" },
+        },
+      }
+    end,
   },
   {
     "saecki/crates.nvim",
