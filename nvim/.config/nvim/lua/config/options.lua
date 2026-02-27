0ea03a73409a61b61f61f6982394ec0b9b10c798
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-02-27 14:31:16 +0530

    build(nvim stuff): Nvim lockfile stuff
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- lua/config/options.lua
+
+vim.g.lazyvim_python_lsp = "ruff" -- Use Ruff LSP server
+vim.g.lazyvim_python_formatter = "ruff_format" -- Ruff for formatting too
