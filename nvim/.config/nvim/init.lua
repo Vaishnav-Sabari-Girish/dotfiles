76aeb70f37b329cd86c15c36f3037abc748d6240
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-07-15 16:22:33 +0530

    feat(play audio): Audio files in `nvim`
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,38 @@ require("config.lazy")
 vim.opt.termguicolors = true
 vim.cmd("colorscheme nord")
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true
+  end,
+})
+
+vim.keymap.set("n", "<leader>m", function()
+  require("fzf-lua").files({
+    prompt = "🎵 Music> ",
+    cwd = vim.fn.expand("~/Music"),
+    previewer = false,
+    cmd = "fd --type f --extension mp3 --extension flac --extension wav --extension ogg",
+    actions = {
+      ["default"] = function(selected, opts)
+        if not selected or #selected == 0 then
+          return
+        end
+
+        -- fzf-lua automatically resolves the absolute path based on opts.cwd
+        local entry = require("fzf-lua").path.entry_to_file(selected[1], opts)
+
+        -- Pass the fully resolved path directly to the player
+        require("nvim.sfx_player").open(entry.path)
+      end,
+    },
+  })
+end, { desc = "Play Music with nvim.sfx_player" })
+
 if vim.g.neovide then
   vim.opt.guifont = "Fira Code:h20"
   vim.opt.termguicolors = true
