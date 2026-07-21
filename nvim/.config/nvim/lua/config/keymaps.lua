ba46456166c74d48471b83352e0f6a916f02c493
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-07-21 10:26:52 +0530

    feat(txm_latex): Latext rendering
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -45,3 +45,29 @@ vim.keymap.set("v", "<leader>d", function()
   -- Spawn the bash script in the background
   vim.fn.jobstart({ vim.fn.expand("~/.config/niri/scripts/define.sh"), word }, { detach = true })
 end, { silent = true, desc = "Define highlighted word" })
+
+vim.keymap.set("n", "<leader>m", function()
+  require("fzf-lua").files({
+    prompt = "🎵 Music> ",
+    cwd = vim.fn.expand("~/Music"),
+    previewer = false,
+    cmd = "fd --type f --extension mp3 --extension flac --extension wav --extension ogg",
+    actions = {
+      ["default"] = function(selected, opts)
+        if not selected or #selected == 0 then
+          return
+        end
+
+        -- fzf-lua automatically resolves the absolute path based on opts.cwd
+        local entry = require("fzf-lua").path.entry_to_file(selected[1], opts)
+
+        -- Pass the fully resolved path directly to the player
+        require("nvim.sfx_player").open(entry.path)
+      end,
+    },
+  })
+end, { desc = "Play Music with nvim.sfx_player" })
+
+-- Latex
+vim.keymap.set({ "v" }, "<leader>tx", "<Plug>(TxmPreview)")
+vim.keymap.set({ "n", "v" }, "<leader>tt", ":TxmToggle")
