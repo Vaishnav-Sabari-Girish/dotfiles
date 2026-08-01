8e4969f2ea7c868f0f3029c1916a67b57cff7f2c
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-08-01 13:24:45 +0530

    feat(terminal): bottom terminal for execution output
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,3 +32,87 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "verilog"
   end,
 })
+
+local function toggle_maximize()
+  vim.cmd("stopinsert")
+  -- If we are in a dedicated full-screen tab, close it to restore previous splits
+  if vim.fn.tabpagenr("$") > 1 and vim.b.is_shell_tab then
+    vim.cmd("tabclose")
+  else
+    -- Otherwise, move the current terminal into a new tab to make it full screen
+    vim.cmd("tab split")
+    vim.b.is_shell_tab = true
+  end
+end
+
+-- Create the custom :Shell command utilizing built-in shell command completion
+vim.api.nvim_create_user_command("Shell", function(opts)
+  local cmd = opts.args
+  if cmd == "" then
+    print("No shell command provided.")
+    return
+  end
+
+  -- 1. Open a horizontal split at the bottom with a height of 12 lines
+  vim.cmd("botright 12split")
+
+  -- 2. Open a terminal running the provided command
+  vim.cmd("terminal " .. cmd)
+
+  local buf = vim.api.nvim_get_current_buf()
+
+  -- Hide the terminal buffer from the buffer list
+  vim.bo[buf].buflisted = false
+
+  -- 3. Define buffer-local keymaps for Normal and Terminal modes
+  local modes = { "n", "t" }
+  local opts_map = { buffer = buf, silent = true }
+
+  -- Toggle full screen smoothly using tabs with 'f'
+  vim.keymap.set(modes, "f", toggle_maximize, opts_map)
+
+  -- Move to Right
+  vim.keymap.set(modes, "r", function()
+    vim.cmd("stopinsert")
+    if vim.fn.tabpagenr("$") > 1 and vim.b.is_shell_tab then
+      vim.cmd("tabclose")
+    end
+    vim.cmd("wincmd L")
+  end, opts_map)
+
+  -- Move to Left
+  vim.keymap.set(modes, "l", function()
+    vim.cmd("stopinsert")
+    if vim.fn.tabpagenr("$") > 1 and vim.b.is_shell_tab then
+      vim.cmd("tabclose")
+    end
+    vim.cmd("wincmd H")
+  end, opts_map)
+
+  -- Move Up
+  vim.keymap.set(modes, "u", function()
+    vim.cmd("stopinsert")
+    if vim.fn.tabpagenr("$") > 1 and vim.b.is_shell_tab then
+      vim.cmd("tabclose")
+    end
+    vim.cmd("wincmd K")
+  end, opts_map)
+
+  -- Move Down
+  vim.keymap.set(modes, "d", function()
+    vim.cmd("stopinsert")
+    if vim.fn.tabpagenr("$") > 1 and vim.b.is_shell_tab then
+      vim.cmd("tabclose")
+    end
+    vim.cmd("wincmd J")
+  end, opts_map)
+
+  -- Automatically enter terminal input mode
+  vim.cmd("startinsert")
+end, {
+  nargs = "*",
+  complete = "shellcmd",
+})
+
+-- Global keymap to open terminal via <leader>T
+vim.keymap.set("n", "<leader>T", ":Shell ", { desc = "Open Shell command terminal" })
